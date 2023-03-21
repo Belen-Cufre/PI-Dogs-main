8b88f09cded29c3c1bca5e496dba370e2b69c10d
Belen-Cufre <belen_cufre@hotmail.com>
2023-03-21 09:29:09 -0300

    Orders OK. Detail and Form pending

--- client/src/components/detail/Detail.jsx
+++ client/src/components/detail/Detail.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import { getDogDetail } from '../../redux/actions';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import Dog from '../home/dog/Dog';
+import { useParams } from 'react-router-dom';
 
 const Detail = () => {
+const dogDetail= useSelector(state => state.dogDetail);
+let id= useParams();
+
+useEffect(() => {
+  getDogDetail(id)
+}, [])
+
+
   return (
-    <div>Detail</div>
+    
+    <div>
+      <h2>{dogDetail.id}</h2>
+      <h3>{dogDetail.height}</h3>    
+      <h3>{dogDetail.temperament}</h3>
+    </div>
+
   )
 }
 
